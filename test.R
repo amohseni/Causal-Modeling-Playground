8b8cdd4f9a5b934a8aafb7864d0a15163d386d43
Aydin <aydin.mohseni@gmail.com>
2017-07-07 13:16:43 -0700

    What, even?

--- test.R
+++ test.R
@@ -1,9 +1,9 @@
-library(prob)
+library(prob)ls
 
 # first row contains variable names, comma is separator 
 # assign the variable id to row names
 # note the / instead of \ on mswindows systems 
 
-mydata <- read.csv("/Users/aydin/GitHub/Test/data2.txt", header=TRUE)
-View(mydata)
-x <- mydata
+x <- read.csv("/Users/aydin/GitHub/Causal-Modeling-Playground/data2.txt", header=TRUE)
+# View(mydata)
+probspace(x, makespace=TRUE)
